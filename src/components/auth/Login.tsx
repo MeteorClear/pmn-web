9f5f602aae98ecd9ea3f48ec1c260a389c821e0a
MeteorClear <tivics9973@gmail.com>
2024-10-11 15:56:14 +0900

    feat: basic setup

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,3 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // 로그인 컴포넌트
+
+interface JwtRequest {
+    username: string;
+    password: string;
+};
+
+const Login = () => {
+    const [credentials, setCredentials] = useState<JwtRequest>({ username: '', password: '' });
+    const [error, setError] = useState<string>('');
+
+    // form 변화에 따른 값 변화
+
+    // 로그인 요청 (POST, /auth/login)
+
+    return (
+        <>
+            <p>Login</p>
+            <form>id/pw/submit</form>
+        </>
+    );
+};
