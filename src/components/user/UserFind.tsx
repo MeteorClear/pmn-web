509efbecb4fd3a34ab3acae6b3d9be3d89276c43
MeteorClear <tivics9973@gmail.com>
2024-10-11 17:07:41 +0900

    feat: add func to find user by username

--- src/components/user/UserFind.tsx
+++ src/components/user/UserFind.tsx
@@ -1,7 +1,15 @@
+import apiClient from "../../api/apiClient"
 
 // 로그인시 사용한 username(email)을 받아서 userid를 찾는 함수
 // get, /users/email/{email}
 
-const getUserByUsername = () => {
-    
-}
+const getUserByUsername = async (email: string) => {
+    try {
+        const response = await apiClient.get(`/users/email/${email}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export default getUserByUsername;
