57d1b8180043da784eac208e36f11fad74717e6d
MeteorClear <tivics9973@gmail.com>
2024-10-14 18:54:17 +0900

    feat: load note detail

--- src/components/note/NoteDetail.tsx
+++ src/components/note/NoteDetail.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import apiClient from "../../api/apiClient";
 
 interface Note {
     id: number;
@@ -11,6 +12,17 @@ const NoteDetail = (noteId: number) => {
     const [note, setNote] = useState<Note | null>(null);
 
     // 노트 정보 Fetch
+    useEffect(() => {
+        const fetchNote = async () => {
+            try {
+                const respnse = await apiClient.get(`/notes/${noteId}`);
+                setNote(respnse.data);
+            } catch (error) {
+                console.error('[ERROR] NoteDetail.tsx ::', error);
+            }
+        };
+        fetchNote();
+    }, [noteId]);
 
     return (
         <div>
