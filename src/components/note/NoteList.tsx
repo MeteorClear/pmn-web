b3488265ea6d42b835287c7a23aa85fd32524dae
MeteorClear <tivics9973@gmail.com>
2024-10-14 17:59:00 +0900

    feat: add showing note list

--- src/components/note/NoteList.tsx
+++ src/components/note/NoteList.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import apiClient from "../../api/apiClient";
 
 interface Note {
     id: number;
@@ -9,8 +10,19 @@ interface Note {
 
 const NoteList = () => {
     const [notes, setNotes] = useState<Note[]>([]);
+    const userId = localStorage.getItem('userId');
 
     // 노트 목록 표시
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const response = await apiClient.get(`/notes/user/${userId}`);
+            } catch (error) {
+                console.error('[ERROR] NoteList.tsx ::', error);
+            }
+        };
+        fetchNotes();
+    }, [userId]);
 
     // 목록 중 클릭한 노트 번호 추출
 
