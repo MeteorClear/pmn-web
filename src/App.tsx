7e91d603e1992039e21ee1831319d98acf14f16c
MeteorClear <tivics9973@gmail.com>
2024-10-15 16:47:52 +0900

    feat: display default comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './components/auth/Login';
+import UserDetail from './components/user/UserDetail';
+import NoteList from './components/note/NoteList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!localStorage.getItem('token'));
+
+    return (
+        <div>
+            {!isAuthenticated ? 
+                <Login /> : 
+                <div>
+                    <UserDetail />
+                    <NoteList />
+                </div>
+            }
+        </div>
+    );
+};
 
 export default App;
