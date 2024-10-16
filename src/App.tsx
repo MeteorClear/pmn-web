671ac3d641900465b0895301c96ab501f90cddba
MeteorClear <tivics9973@gmail.com>
2024-10-16 17:08:53 +0900

    feat: add logout rendering

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './components/auth/Login';
 import UserDetail from './components/user/UserDetail';
 import NoteList from './components/note/NoteList';
+import Logout from './components/auth/Logout';
 
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!localStorage.getItem('token'));
@@ -12,11 +13,17 @@ const App = () => {
         setIsAuthenticated(true);
     }
 
+    // 로그아웃시 false
+    const handleLogout = () => {
+        setIsAuthenticated(false);
+    }
+
     return (
         <div>
             {!isAuthenticated ? 
                 <Login onLogin={handleLogin} /> : 
                 <div>
+                    <Logout onLogout={handleLogout} />
                     <UserDetail />
                     <NoteList />
                 </div>
