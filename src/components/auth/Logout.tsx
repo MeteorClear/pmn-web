2ca43a8d4ecb63f4a790d86daee760b7260238b6
MeteorClear <tivics9973@gmail.com>
2024-10-16 17:08:06 +0900

    feat: add logout

---
+++ src/components/auth/Logout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface LogoutProps {
+    onLogout: () => void;
+}
+
+const Logout = ({ onLogout }: LogoutProps) => {
+
+    // 로그아웃시 데이터 삭제
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        onLogout();
+        alert('logout succeeded');
+    };
+
+    return (
+        <div>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    );
+};
+
+export default Logout;
