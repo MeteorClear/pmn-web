831afe6f698d6ea591f2c6b867cea7019772b317
MeteorClear <tivics9973@gmail.com>
2024-10-16 17:58:16 +0900

    add: Register

---
+++ src/components/auth/Register.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface UserRegisterRequest {
+    email: string;
+    password: string;
+    usernsme: string;
+};
+
+const Register = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
