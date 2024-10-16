673ffb12c35169b361b3a647dec66e788e0bac31
MeteorClear <tivics9973@gmail.com>
2024-10-16 18:13:16 +0900

    feat: update input value

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { HtmlHTMLAttributes, useState } from "react";
 
 interface UserRegisterRequest {
     email: string;
     password: string;
-    usernsme: string;
+    username: string;
 };
 
 const Register = () => {
+    const [user, setUser] = useState<UserRegisterRequest>({
+        email: '',
+        password: '',
+        username: ''
+    });
+    const [error, setError] = useState<string | null>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
 
     return (
         <div>
             <p>Register</p>
             <form>
-                <input />
-                <input />
-                <input />
+                <input 
+                    type="email"
+                    name="email"
+                    value={user.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                    required
+                />
+                <input 
+                    type="password"
+                    name="password"
+                    value={user.password}
+                    onChange={handleChange}
+                    placeholder="Password"
+                    required
+                />
+                <input 
+                    type="text"
+                    name="username"
+                    value={user.username}
+                    onChange={handleChange}
+                    placeholder="Username"
+                    required
+                />
                 <button type="submit">Register</button>
             </form>
         </div>
