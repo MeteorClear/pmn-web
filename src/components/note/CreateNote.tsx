6d285bead97be282696b241095dd0cc5b05fd753
MeteorClear <tivics9973@gmail.com>
2024-10-17 16:52:35 +0900

    add: create CreateNote

---
+++ src/components/note/CreateNote.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface CreateNoteRequest {
+    title: string;
+    content: string;
+};
+
+const CreateNote = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CreateNote;
