2457ecafc30157573a02cf4dbff82e698c3b9efb
MeteorClear <tivics9973@gmail.com>
2024-10-21 18:01:32 +0900

    design: add register

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from './components/auth/Login';
 import UserDetail from './components/user/UserDetail';
 import NoteList from './components/note/NoteList';
 import Logout from './components/auth/Logout';
+import Register from './components/auth/Register';
 
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!localStorage.getItem('token'));
@@ -21,7 +22,11 @@ const App = () => {
     return (
         <div>
             {!isAuthenticated ? 
-                <Login onLogin={handleLogin} /> : 
+                <div>
+                    <Login onLogin={handleLogin} />
+                    <Register />
+                </div>
+                : 
                 <div>
                     <Logout onLogout={handleLogout} />
                     <UserDetail />
