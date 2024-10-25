cd7c4121aabec368c1dda8fa8c2c139095429a9d
MeteorClear <tivics9973@gmail.com>
2024-10-25 16:57:23 +0900

    feat: remove props

--- src/components/user/DeleteUser.tsx
+++ src/components/user/DeleteUser.tsx
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import apiClient from "../../api/apiClient";
+import { useNavigate } from "react-router-dom";
 
-interface DeleteUserProps {
-    userId: number;
-};
-
-const DeleteUser = ({ userId }: DeleteUserProps ) => {
+const DeleteUser = () => {
+    const [userId, setUserId] = useState<number | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const storedUserId = localStorage.getItem('userId')
+            if (!storedUserId) {
+                alert('User load failed, please relogin');
+                console.error('[ERROR] DeleteUser.tsx:: no userId found');
+                navigate('/login');
+                return;
+            }
+
+            setUserId(Number(storedUserId));
+        };
+        fetchUser();
+    }, [userId, navigate]);
 
     const handleDelete = async () => {
         const confirmDelete = window.confirm('Do you really want to delete?');
