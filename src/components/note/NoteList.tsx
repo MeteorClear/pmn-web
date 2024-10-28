799fe13ee459bf1d29214298c5a0fb40e1aeff52
MeteorClear <tivics9973@gmail.com>
2024-10-28 16:45:07 +0900

    design: set note item layout

--- src/components/note/NoteList.tsx
+++ src/components/note/NoteList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import apiClient from "../../api/apiClient";
 import NoteDetail from "./NoteDetail";
 import CreateNote from "./CreateNote";
+import styles from './NoteList.module.css';
 
 interface Note {
     id: number;
@@ -31,19 +32,18 @@ const NoteList = () => {
     // 목록 중 클릭한 노트 번호 추출
 
     return (
-        <div style={{border: '2px solid black'}}>
-            <div>
-                <p>Note List...</p>
+        <div className={styles.container}>
+            <div className={styles.noteListBox}>
                 {notes.map((note) => (
-                    <div key={note.id} onClick={() => setSelectedNote(note.id)}>
+                    <div className={styles.noteItem} key={note.id} onClick={() => setSelectedNote(note.id)}>
                         {note.title}
+                        <br />
+                        {note.createdAt}
                     </div>
                 ))}
             </div>
             <div>
                 <CreateNote />
-            </div>
-            <div>
                 {selectedNote && <NoteDetail noteId={selectedNote} />}
             </div>
         </div>
